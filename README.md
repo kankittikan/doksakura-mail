612e89ef01f85fc9cd12e0c494e7d1eec9b9b7cb
kankittikan <nongkan195@gmail.com>
2024-02-27 22:02:11 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# doksakura-mail
---
+++ src/main/java/com/doksakura/doksakuramail/DoksakuraMailApplication.java
@@ -0,0 +1,13 @@
+package com.doksakura.doksakuramail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoksakuraMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoksakuraMailApplication.class, args);
+	}
+
+}
